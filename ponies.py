4e43e20ccef74014c2178a51d4c0e0799f72f3aa
dgporter90 <dgporter@gmail.com>
2017-06-06 17:05:56 -0500

    Added funtion for bullet workouts, and printing results file line by line so it is easier to read.

--- ponies.py
+++ ponies.py
@@ -17,7 +17,7 @@ results = []
 
 # Ask for file path
 #filepath = raw_input("Enter file path:")
-filepath = "/Users/DavidPorter/Desktop/HorseRacing/DataFiles/CDX0506.csv"
+filepath = "/Users/DavidPorter/Desktop/HorseRacing/DataFiles/PIM0520.csv"
 filepath = filepath.replace(" ","")
 
 # Check if file path is correct
@@ -42,22 +42,41 @@ def winpct(starts, wins, i):
 	else:
 		results[i].append(float(wins)/float(starts)*100)
 
-#Horse Lasix today
+# Horse Lasix today
 def lasix(meds, i):
 	if int(meds) == 1 or int(meds) == 3 or int(meds) == 4 or int(meds) == 5:
 		results[i].append("YES LASIX")
 	else:
 		results[i].append("no lasix")
 
+# Recent bullet workouts
+def bullets(wo1, wo2, wo3, i):
+	total = 0
+	if wo1:
+		if float(wo1) < 0:
+			total += 1
+	if wo2:
+		if float(wo2) < 0:
+			total += 1
+	if wo3:
+		if float(wo3) < 0:
+			total += 1
+
+	results[i].append(str(total) + " Bullets")
+
 
 # Loop through array and perform calculations, append to results array
 for i, line in enumerate(ppfile):
 	results.append([ppfile[i][44]])
 
-	winpct(ppfile[i][96],ppfile[i][97],i)
-	lasix(ppfile[i][61],i)
+	winpct(ppfile[i][96], ppfile[i][97], i)
+	lasix(ppfile[i][61], i)
+	bullets(ppfile[i][113], ppfile[i][114], ppfile[i][115], i)
 
-print results
+for i, line in enumerate(results):
+	print results[i]
+
 print
+
 
 # Next up, start data analysis. Functions? Class? Think about best way to solve.
